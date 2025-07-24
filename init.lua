123691e5c608d74646d398c12c2e4d5543002428
nikita <126313574+ere6u5@users.noreply.github.com>
2025-07-25 00:06:42 +0400

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- Basic settings
+vim.opt.listchars = { trail = '•' }
+vim.opt.tabstop = 4
+vim.opt.number = true
+
+-- Additional
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
